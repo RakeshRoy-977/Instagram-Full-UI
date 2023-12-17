76b1a74580ab5b506bef5264d5a546847b253e67
RakeshRoy-977 <15rakeshroy@gmail.com>
2023-12-17 19:31:40 +0530

    half complete

--- src/Components/data.js
+++ src/Components/data.js
@@ -24,3 +24,45 @@ export const userdata = [
     following: 69,
   },
 ];
+
+export const feedPostData = [
+  {
+    avatarPic: "Flex.jpeg",
+    userName: "Rakesh.roy.977",
+    postTime: "3d",
+    likes: 851,
+    postImg: "Flex.jpeg",
+    postDis:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, voluptates...",
+  },
+  {
+    avatarPic: "",
+    userName: "PixelPioneer84",
+    postTime: "2w",
+    likes: 541,
+    postImg:
+      "https://scontent.cdninstagram.com/v/t39.30808-6/409890527_18270402817160957_7153540885270249380_n.jpg?stp=dst-jpg_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=105&_nc_ohc=EpRCbnZIuRQAX948BQi&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzI1ODA0OTI3ODk1MzU0NTcxOQ%3D%3D.2-ccb7-5&oh=00_AfB20B9dc0Wgj7OPI43Uh1IGzzT6mK7dyHoW88xgXBDa8g&oe=6584D550&_nc_sid=10d13b",
+    postDis:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, voluptates...",
+  },
+  {
+    avatarPic: "",
+    userName: "TechSavvyWhiz",
+    postTime: "6h",
+    likes: 153,
+    postImg:
+      "https://scontent.cdninstagram.com/v/t39.30808-6/410385123_764530052366088_1599292031125727973_n.jpg?stp=dst-jpg_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNjM4eDIwNDguc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=100&_nc_ohc=LsE_rxXlubAAX8yIgFX&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzI1ODI2NDIxNjY1NzEzNzk2NA%3D%3D.2-ccb7-5&oh=00_AfCgOftSX5Uy1V9swr26-u_eiw9gGoVnsC0V7p2cKkbUiQ&oe=658471AF&_nc_sid=10d13b",
+    postDis:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, voluptates...",
+  },
+  {
+    avatarPic: "",
+    userName: "AquaZephyr39",
+    postTime: "4h",
+    likes: 57,
+    postImg:
+      "https://plus.unsplash.com/premium_photo-1674057858191-c2de80d6049a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    postDis:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, voluptates...",
+  },
+];
--- src/Page/FeedPage.jsx
+++ src/Page/FeedPage.jsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import Sidebar from "../Components/Sidebar";
+import SuggestUsers from "../Components/SuggestUsers";
+import "remixicon/fonts/remixicon.css";
+import { feedPostData } from "../Components/data";
 
 const FeedPage = () => {
+  const [like, setLike] = useState(true);
+
+  function handelLike() {
+    setLike(!like);
+  }
+
   return (
     <>
-      <div className="bg-black h-[100vh] w-[100vw]"></div>
+      <div className="bg-black text-white ">
+        <div className="flex gap-20 border-2 border-yellow-500 justify-evenly ">
+          <Sidebar />
+          <div className="feedPage ">
+            {feedPostData.map((x) => (
+              <div className="flex flex-col  mb-10 mt-10 gap-2 flex-wrap w-[30vw]">
+                <div className="flex gap-3 items-center">
+                  <img className="rounded-full w-10 h-10" src={x.avatarPic} />
+                  <p>{x.userName}</p>
+                  <p>{x.postTime}</p>
+                </div>
+                <div>
+                  <img
+                    className="h-[40rem] w-[33rem]  rounded-md"
+                    src={x.postImg}
+                    alt="posted pic"
+                  />
+                </div>
+                <div className="flex gap-3 ">
+                  <div onClick={handelLike} className="liked">
+                    {like ? (
+                      <i className="ri-heart-fill text-[30px] cursor-pointer"></i>
+                    ) : (
+                      <i className="ri-heart-line text-[30px] cursor-pointer"></i>
+                    )}
+                  </div>
+
+                  <i className="ri-chat-1-line text-[30px] cursor-pointer"></i>
+                  <i className="ri-send-plane-2-line text-[30px] cursor-pointer"></i>
+                </div>
+                <p className="text-gray-300">{x.likes} likes</p>
+                <p className="text-gray-300">
+                  {x.userName} ~ {x.postDis}
+                </p>
+                <input
+                  className="bg-transparent border-b-2 border-b-gray-700"
+                  type="text"
+                  placeholder="Add a comment..."
+                />
+              </div>
+            ))}
+          </div>
+          <SuggestUsers />
+        </div>
+      </div>
     </>
   );
 };
